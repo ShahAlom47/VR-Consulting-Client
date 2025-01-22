686db7784bf750d5c3ef2f97ae68becb30d91857
ShahAlom47 <sahalom4729@gmail.com>
2025-01-22 23:04:51 +0600

    added navbar

--- src/SharedComponent/Navbar.jsx
+++ src/SharedComponent/Navbar.jsx
@@ -1,9 +1,35 @@
+import { NavLink } from "react-router";
+import logo from '../Accets/Images/logo.png'
+import PrimaryButton from "./PrimaryButton";
 
 
 const Navbar = () => {
+    const nav = [
+
+       
+        <NavLink key={'LOGO'} to={'/'} className={''} ><img src={logo} className=" w-8" alt="logo" /> </NavLink>,
+        <NavLink key={'HOME'} to={'/'} className={'rounded-lg p-2 px-3 hover:bg-black'} >Home </NavLink>,
+        <NavLink key={'FAQ'} to={'/'} className={'rounded-lg p-2 px-3 hover:bg-black'} >FAQ </NavLink>,
+        <NavLink key={'contact'} to={'/'} className={'rounded-lg p-2 px-3 hover:bg-black'} >Contact </NavLink>,
+        <NavLink key={'button'} className={''} > <PrimaryButton>Book a call</PrimaryButton></NavLink>
+
+       
+    ]
+
+
     return (
-        <div>
-            navbar
+        <div className=" bb p-2  my-4 bg-white">
+            <div className=" mx-auto max-w-lg  flex gap-4 ">
+                <div className="p-1 border rounded-xl flex-1 bg-[#0b0a0ac2] flex gap-2 justify-between  items-center text-white font-semibold">
+                    {nav}
+
+                </div>
+                <div className="p-2 border rounded-lg">
+
+                </div>
+               
+
+            </div>
         </div>
     );
 };
---
+++ src/SharedComponent/PrimaryButton.jsx
@@ -0,0 +1,13 @@
+ 
+
+// eslint-disable-next-line no-unused-vars, react/prop-types
+const PrimaryButton = ({children}) => {
+    return (
+       <button type="button" className=" bg-[#ff891d] rounded-lg p-2 px-3 text-black   ">
+        {children}
+
+       </button>
+    );
+};
+
+export default PrimaryButton;
